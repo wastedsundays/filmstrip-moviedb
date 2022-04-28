017b65cf0a76c666a64e634e9c7c077304f10c66
Adam Hauck <ad.hauck@gmail.com>
2022-04-28 09:34:26 -0700

    Fixed bottom margin on nav section

---
+++ src/pages/PageHome.js
@@ -0,0 +1,15 @@
+// import SectionTitle from '../components/SectionTitle';
+// import MovieScrollerSection from '../components/MovieScrollerSection';
+
+const PageHome = () => {
+    return(
+        <section className='home-page'>
+        {/* <SectionTitle />
+        <MovieScrollerSection /> */}
+        <h1>Home Page</h1>
+        <p>ipsum ipsum ipsum</p>
+        </section>
+    )
+}
+
+export default PageHome;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,7 +7,7 @@ import Footer from '../components/Footer';
 // import PageAbout from '../pages/PageAbout.js';
 // import PageAccount from '../pages/PageAccount.js';
 // import PageFavourites from '../pages/PageFavourites.js';
-// import PageHome from '../pages/PageHome.js';
+import PageHome from '../pages/PageHome.js';
 // import PageSingle from '../pages/PageSingle.js';
 // import Page404 from '../pages/Page404.js';
 
@@ -17,14 +17,14 @@ function App() {
       <BrowserRouter>
         <div>
         <Header />
-        {/* <Routes>
+        <Routes>
           <Route path='/' element={<PageHome />} />
-          <Route path='/favourites' element={<PageFavourites />} />
+          {/* <Route path='/favourites' element={<PageFavourites />} />
           <Route path='/account' element={<PageAccount />} />
           <Route path='/about' element={<PageAbout />} />
           <Route path='/single' element={<PageSingle />} />
-          <Route path='*' element={<Page404 />} />
-        </Routes> */}
+          <Route path='*' element={<Page404 />} /> */}
+        </Routes> 
         <Footer /> 
         </div>
       </BrowserRouter>
