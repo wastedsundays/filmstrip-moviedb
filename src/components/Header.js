48e3192ab64ac659601140497fc0c87b01134a3e
Adam Hauck <ad.hauck@gmail.com>
2022-05-08 00:40:17 -0700

    single movie page

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,14 +12,14 @@ const Header = () => {
                 <NavLink to="/"><img src={logo} className='logo' alt='FilmsTrip Logo' /></NavLink>          
                 <nav className='header-nav'> 
                     <Hamburger />                   
-                    <ul className='nav-menu'>                             
+                    {/* <ul className='nav-menu'>                             
                         <li className='spacer'></li>
                         <li><NavLink to="/">Home</NavLink></li>
                         <li><NavLink to="/favourites">Favourites</NavLink></li>
                         <li><NavLink to="/account">Account</NavLink></li>
                         <li><NavLink to="/about">About</NavLink></li>
                         <li className='dark-light-mode'></li>
-                    </ul>
+                    </ul> */}
                     
                 </nav>
             </div>
