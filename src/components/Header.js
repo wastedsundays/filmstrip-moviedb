32ce663914a4dd9c00188c08030ff2db1c709eb3
Adam Hauck <ad.hauck@gmail.com>
2022-05-11 23:49:31 -0700

    fixed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import {NavLink} from 'react-router-dom';
 import React, {useState, useEffect} from 'react'
-import logo from '../images/filmstrip-logo-white.png';
+import logo from '../images/filmstrip-logo-white2.png';
 import SearchBar from './Search';
 // import Hamburger from './Hamburger';
 import {Divide as Hamburger} from 'hamburger-react';
@@ -9,9 +9,11 @@ import {Divide as Hamburger} from 'hamburger-react';
 const Header = () => {
     const [toggleMenu, setToggleMenu] = useState(false)
     const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+    const [isOpen, setOpen] = useState(false)
 
     const toggleNav = () => {
         setToggleMenu(!toggleMenu)
+     
       }
 
     useEffect(() => {
@@ -33,7 +35,7 @@ const Header = () => {
                 <NavLink to="/"><img src={logo} className='logo-mobile' alt='FilmsTrip Logo' /></NavLink>          
                 <nav className='header-nav'> 
                     {(toggleMenu || screenWidth > 799) && (
-                        <ul className='nav-menu'>                             
+                        <ul className='nav-menu' onClick={toggleNav}>                             
                             <li className='spacer'></li>
                             <li><NavLink to="/">Home</NavLink></li>
                             <li><NavLink to="/favourites">Favourites</NavLink></li>
