17efc191031cb2b28bb845cabb163f71ccd75d95
Adam Hauck <ad.hauck@gmail.com>
2022-05-13 09:31:20 -0700

    switching header

---
+++ src/components/Header-new.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+import Nav from './Nav';
+// import { appTitle } from '../globals/globals';
+
+const Header = () => {
+
+    const [navOpen, setNavOpen] = useState(false);
+
+    const showHideNav = () => {
+        setNavOpen(!navOpen);
+    }
+   
+    const isDesktop = (e) => {
+        if(e.matches){
+            setNavOpen(false);
+        }
+    }
+    
+    useEffect(() => {
+          let mediaQuery = window.matchMedia('(min-width: 600px)');
+          mediaQuery.addEventListener('change', isDesktop);
+          // this is the cleanup function to remove the listener
+          return () => mediaQuery.removeEventListener('change', isDesktop);
+    }, []);
+
+    return (
+        <header className={navOpen ? 'show' : undefined}>
+            {/* <h1><Link to="/">{appTitle}</Link></h1> */}
+            {/**
+             * HTML for the Hamburger icon modified from HTMl 
+             * found at this codepen:
+             * https://codepen.io/RRoberts/pen/ZBYaJr
+             */}
+            <button className="btn-main-nav" 
+                    onMouseDown={(e) => { e.preventDefault(); }}
+                    onClick={showHideNav}>
+                <span className="hamburger-icon">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </span>
+                <span className="sr-only">Menu</span>
+            </button>
+            <Nav handleShowHideNav={showHideNav} />
+        </header>
+    );
+
+};
+
+export default Header;
--- src/globals/globals.js
+++ src/globals/globals.js
@@ -1,2 +1,2 @@
 export const API_KEY = 'e2be774394fd67e79142c1318cddcc84';
-
+export const appTitle = 'FilmsTrip...';
