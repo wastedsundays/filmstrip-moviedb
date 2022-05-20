a4aa0d3fd984e198c0946e0d42ce2ad2527c307c
Adam Hauck <ad.hauck@gmail.com>
2022-05-19 22:05:23 -0700

    about styling

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -6,22 +6,23 @@ const PageAbout = () => {
     return(
         <section className='about-page'>
             <div className="about-box">
-                <h1>About</h1>
+                <h1>Have A Nice Trip</h1>
                 <article>
                     <p>Let's take a trip through the films, shall we?</p> 
-                    <p>Films Trip is the internet movie database.</p> <p>On the main page, you can choose to view the top movies for each category: Popular movies, New Releases, Upcoming releases and Top Rated.</p>
+                    <p>FilmsTrip is the internet movie database.</p> <p>On the main page, you can choose to view the top movies for each category: Popular movies, New Releases, Upcoming releases and Top Rated.</p>
                     <p>You can view more details by selecting a movie, which will take you to the individual movie page.</p> 
                     <p>Add a movie to your favourites list by clicking on it's star when browsing through the database. 
                     </p>
                 </article>
                 <h2>Credits</h2>
+
+                    <article>
+                        <p>FilmsTrip was created, designed, and coded by Adam H.</p>
+                        <p>&copy; {getYear()} <a href="https://www.adamh.ca">Adam H.</a></p>
+                    </article>
                     <article>
                         <p>This product uses the TMDB API but is not endorsed or certified by TMDB.</p>
                         <img src={moviedbLogo} alt="The Movie DB" />
-                    </article>
-                    <article>
-                        <p>FilmsTrip was created, designed, and coded by Adam H.</p>
-                        <p>&copy; {getYear()} Adam H.</p>
                     </article>
 
             </div>
