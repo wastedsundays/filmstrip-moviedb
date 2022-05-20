188ae684c14a34026acd6cb96d30d518f7452a58
Adam Hauck <ad.hauck@gmail.com>
2022-05-19 22:00:34 -0700

    about page & styling

--- src/globals/globals.js
+++ src/globals/globals.js
@@ -1,2 +1,9 @@
 export const API_KEY = 'e2be774394fd67e79142c1318cddcc84';
 export const appTitle = 'FilmsTrip...';
+
+function getYear(){
+    const d = new Date();
+    return d.getFullYear();
+}
+
+export { getYear };
--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,9 +1,29 @@
+import moviedbLogo from '../images/moviedb-logo.svg';
+import { getYear } from '../globals/globals';
+
+
 const PageAbout = () => {
     return(
         <section className='about-page'>
-            <div className='section-title'>
+            <div className="about-box">
                 <h1>About</h1>
-            
+                <article>
+                    <p>Let's take a trip through the films, shall we?</p> 
+                    <p>Films Trip is the internet movie database.</p> <p>On the main page, you can choose to view the top movies for each category: Popular movies, New Releases, Upcoming releases and Top Rated.</p>
+                    <p>You can view more details by selecting a movie, which will take you to the individual movie page.</p> 
+                    <p>Add a movie to your favourites list by clicking on it's star when browsing through the database. 
+                    </p>
+                </article>
+                <h2>Credits</h2>
+                    <article>
+                        <p>This product uses the TMDB API but is not endorsed or certified by TMDB.</p>
+                        <img src={moviedbLogo} alt="The Movie DB" />
+                    </article>
+                    <article>
+                        <p>FilmsTrip was created, designed, and coded by Adam H.</p>
+                        <p>&copy; {getYear()} Adam H.</p>
+                    </article>
+
             </div>
         </section>
     )
