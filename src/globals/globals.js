6c714f276151fa857d740620b10be01f78d636a6
Adam Hauck <ad.hauck@gmail.com>
2022-07-18 19:03:47 -0700

    added const for localstorage Name

--- src/globals/globals.js
+++ src/globals/globals.js
@@ -1,5 +1,6 @@
 export const API_KEY = 'e2be774394fd67e79142c1318cddcc84';
 export const appTitle = 'FilmsTrip...';
+export const appStorageName = 'FilmsTrip-favs-list';
 
 function getYear(){
     const d = new Date();
