79c01fe235d2606bfacba8eb4921ad64a54fd372
Adam Hauck <ad.hauck@gmail.com>
2022-07-24 02:59:48 -0700

    added account page with temp content

--- src/pages/PageAccount.js
+++ src/pages/PageAccount.js
@@ -1,12 +1,30 @@
-const PageAccount = () => {
+import { useSelector } from 'react-redux';
+import MovieCard from '../components/MovieCard'; 
+// import NoFavs from '../components/NoFavs';
+import Footer from '../components/Footer';
+
+const PageAccount = (onFavsPage) => {
+
+    const favouriteItems = useSelector((state) => state.favs.items);
+
     return(
         <section className='account-page'>
             <div className='section-title'>
-                <h1>Account Page</h1>
+                <h1>My Films Trip</h1>
                 <hr/>
+                <div className='movies-container'>
+                {favouriteItems.length === 0 ?
+                //  <NoFavs/> 
+                 <Footer />
+                 : favouriteItems.map(movie => 
+                    <li key={movie.id}>{movie.title}</li>)}
+            </div>
             </div>
         </section>
     )
 }
 
 export default PageAccount;
+
+
+
