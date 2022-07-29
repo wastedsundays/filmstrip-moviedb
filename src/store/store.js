c13c69e5e1119eddcfe3645be527a02490afb55e
Adam Hauck <ad.hauck@gmail.com>
2022-07-28 23:03:37 -0700

    added watch to store and is watch

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import favsReducer from '../features/favs/favsSlice';
+import watchReducer from '../features/watch/watchSlice';
 
 export const store = configureStore({
   reducer: {
     favs: favsReducer,
+    watch: watchReducer,
   }
 });
---
+++ src/utilities/isWatch.js
@@ -0,0 +1,20 @@
+function isWatch(arr, path, id){
+
+    // If we are on the watch later page...then no 
+    // need to check for what is added to watch as they all set to watch later
+    if(path === '/account'){
+        return true;
+    }
+  
+    // If there is nothing added to watch later...
+    // then no need to check if movie has been added.
+    if(arr.length === 0){
+      return false;
+    }
+  
+    // Checks whether the object is favourited
+    return arr.some((obj) => obj.id === id);
+  
+  }
+  
+  export default isWatch;
