605215fdf827d57aa3c91e86b23007a463f72716
Adam Hauck <ad.hauck@gmail.com>
2022-07-30 15:41:28 -0700

    added watched icon to movie card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,19 +1,25 @@
 import { Link } from 'react-router-dom';
 import noPoster from '../images/no-movie-poster.jpg';
-import { FaStar } from 'react-icons/fa';
-import { FaRegStar } from 'react-icons/fa';
+import { FaStar, FaRegStar, FaEye, FaRegEye } from 'react-icons/fa';
+// import { FaRegStar } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { addItem, deleteItem } from '../features/favs/favsSlice';
+import { addWatchItem, deleteWatchItem } from '../features/watch/watchSlice';
 
 
-function MovieCard({ movie, onFavsPage }) {
+function MovieCard({ movie, onFavsPage, onWatchPage }) {
 
   const favouriteItems = useSelector((state) => state.favs.items);
+  const watchLater = useSelector((state) => state.watch.items);
   const dispatch = useDispatch();
 
   function inFav(id, arr){
       return arr.some(item => item.id === id);
   }
+
+  function inWatch(id, arr){
+    return arr.some(item => item.id === id);
+}
 
   return (
     <div className="movie-card">
@@ -27,16 +33,17 @@ function MovieCard({ movie, onFavsPage }) {
             <h2 className="movie-card-title">{movie.title.length > 35 ? `${movie.title.substring(0, 35)}...` : movie.title}</h2>
 
             <div className='moviecard-rating-section'> 
-              <h3>{movie.vote_average}</h3>
-{/* use this for when we add the watch history / films trip feature */}
-              {/* {(onFavsPage === true || inFav(movie.id, favouriteItems) === true ) ? 
-                <div className='is-favs-icon' onClick={() => dispatch(deleteItem(movie))}><FaStar color="red"/></div> : 
-                <div className='add-fav-icon-container' onClick={() => dispatch(addItem(movie))}><FaRegStar /></div>
-              } */}
+
 
               {(onFavsPage === true || inFav(movie.id, favouriteItems) === true ) ? 
                 <div className='is-favs-icon' onClick={() => dispatch(deleteItem(movie))}><FaStar color="red" title="In Your Favourites List"/></div> : 
                 <div className='add-fav-icon-container' onClick={() => dispatch(addItem(movie))}><FaRegStar title="Add to Your Favourites list"/></div>
+              }
+
+              <h3>{movie.vote_average}</h3>
+              {(onWatchPage === true || inWatch(movie.id, watchLater) === true ) ? 
+                  <div className='is-watch-icon' onClick={() => dispatch(deleteWatchItem(movie))}><FaEye color="red" title="You have watched this"/></div> : 
+                  <div className='add-watch-icon-container' onClick={() => dispatch(addWatchItem(movie))}><FaRegEye title="Add to your Trip"/></div>
               }
             </div>
             
